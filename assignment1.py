3996e8d9901725b58eb0adf9999100aae7e51582
Matt Henry <eldushbago@gmail.com>
2014-09-03 20:16:13 -0400

    my bad

--- assignment1.py
+++ assignment1.py
@@ -26,10 +26,9 @@ class Rememberer:
 		for e in templist:
 			if ( saveNext ):
 				self.remeberthese.add(e)
-				saveNext = False
+			saveNext = self.check4to9long(e)
 			if (self.checkprime(e)):
 				self.remeberthese.add(e)
-				saveNext = self.check4to9long(e)
 				continue
 			if (self.checkthreechar(e)):
 				self.remeberthese.add(e)
